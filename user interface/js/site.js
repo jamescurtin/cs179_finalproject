d9535e990fb16a5c6b5ff583588a34570c818fb9
Enrique D Meneses <enrique.a.meneses@gmail.com>
2015-04-06 23:02:05 -0400

    select item complete and other edits

--- user interface/js/site.js
+++ user interface/js/site.js
@@ -16,3 +16,16 @@ function show(id){
 function hide(id){
     $( "#" + id ).addClass("hidden");
 }
+
+
+//detect if sizes is required
+function getsize(item, id){
+    item = item.split("_");
+    id = "select-" + id + "-size";
+    if(item[item.length - 1] == "s"){
+        $( "#" + id ).removeClass("hidden");
+    }
+    else{
+        $( "#" + id ).addClass("hidden");
+    }  
+}
