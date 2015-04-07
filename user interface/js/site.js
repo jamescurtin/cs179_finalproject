9dbc1707ee52d19c21fd7a5c6e58ad5a1aec8da9
Enrique D Meneses <enrique.a.meneses@gmail.com>
2015-04-06 20:34:57 -0400

    settings complete

--- user interface/js/site.js
+++ user interface/js/site.js
@@ -6,3 +6,13 @@
 (window.onload = function (){   
     console.log(1);
 });
+
+//shows hidden div by id
+function show(id){
+    $( "#" + id ).removeClass("hidden");
+}
+
+//shows hidden div by id
+function hide(id){
+    $( "#" + id ).addClass("hidden");
+}
