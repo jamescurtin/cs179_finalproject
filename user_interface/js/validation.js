d7f7eaa4390486a29197abac25ba9e832226b7aa
Enrique D Meneses <enrique.a.meneses@gmail.com>
2015-04-12 23:42:09 -0400

    val edit

--- user_interface/js/validation.js
+++ user_interface/js/validation.js
@@ -8,6 +8,7 @@
 var page = {"register-form":"credit_card",
            "creditcard-form":"welcome",
            "login-form":"home_screen",
+           "select_item-form":"check_out.html",
            }
 // function to execute
 var fx = {"login-form": login, "register-form": login, 
