bb7203c84ce7dc3f99803e7a7fe725018f6d8cd4
Wilson Qin <wilson.qin@gmail.com>
2015-04-14 18:18:31 -0400

    load search index into data model

--- user_interface/js/LEDataModel.js
+++ user_interface/js/LEDataModel.js
@@ -4,12 +4,17 @@
  */
 
 (function(){
-  var restaurants;
+  var restaurants,
+    searchIndex;
 
   // init restaurants data
   // the return value of getJSON will be used for synchronization as a Deferred Object
   var loadingRestaurants = $.getJSON( "js/data.json", function( data ) {
     restaurants = data;
+  });
+
+  var loadingSearchIndex = $.getJSON("js/search.json", function(data){
+    searchIndex = data;
   });
 
   // returns a restaurant obj or all restaurants
@@ -20,11 +25,23 @@
     return restaurants[key];
   }
 
+  // takes a search term and returns a list of restaurant ids that match the keyword
+  function getRestaurantsBySearch(keyword){
+    if(keyword in searchIndex){
+      return searchIndex[keyword];
+    }else{
+      return [];
+    }
+  }
+
   window.LE = {
     restaurants: {
-      getRestaurant: getRestaurant
+      getRestaurant: getRestaurant,
+      getRestaurantsBySearch: getRestaurantsBySearch
     },
     loadingRestaurants: loadingRestaurants,
+
+    // user data can be stored here
     userData: {
       currentRestaurant: null,
       items: null,
